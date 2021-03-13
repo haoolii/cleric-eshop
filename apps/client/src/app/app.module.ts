2fd68f761294fe1fc60309ed7f12a0532b01af1a
unnhao <unnhao@gmail.com>
2021-03-13 23:30:41 +0800

    feat: separate client foot and header

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { RootComponent, HomeComponent } from './pages';
-
+import { ClientUiModule } from '@cleric-eshop/client-ui';
 @NgModule({
   declarations: [AppComponent, RootComponent, HomeComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, ClientUiModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
     '<rootDir>/apps/api',
     '<rootDir>/apps/admin',
     '<rootDir>/libs/material',
+    '<rootDir>/libs/client-ui',
   ],
 };
