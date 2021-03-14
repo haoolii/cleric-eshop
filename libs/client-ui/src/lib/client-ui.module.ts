81d3e5bdf2b8189c2456881a6039c3927dc9bbf1
unnhao <unnhao@gmail.com>
2021-03-14 14:30:54 +0800

    refact: remove lib header and footer

--- libs/client-ui/src/lib/client-ui.module.ts
+++ libs/client-ui/src/lib/client-ui.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HeaderComponent, FooterComponent],
-  exports: [HeaderComponent, FooterComponent]
+  declarations: [],
+  exports: []
 })
 export class ClientUiModule {}
