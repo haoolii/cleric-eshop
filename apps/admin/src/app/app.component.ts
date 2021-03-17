3dc1cbf7b14c9ab544da88acb8beb9ea27c00fc0
Hao <Hao@miniasp.com>
2021-03-17 15:23:36 +0800

    feat: Temp

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cleric-eshop-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'admin';
+  isCollapsed = false;
 }
