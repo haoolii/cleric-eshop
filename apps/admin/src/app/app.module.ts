e6bae51f866e2189a093a1a3177f01328dfabc1f
Hao <Hao@miniasp.com>
2021-03-17 15:23:36 +0800

    feat: Temp

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -2,18 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from '@cleric-eshop/material';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_TW } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { NgZorroModule } from '@cleric-eshop/ng-zorro';
+
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgZorroModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_TW }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
