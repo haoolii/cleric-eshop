cb3a204dd501c6b3205f4978e0a59ded198129a8
Hao <Hao@miniasp.com>
2021-03-24 10:56:37 +0800

    feat: create category-selector

--- libs/admin-ui/src/lib/admin-ui.module.ts
+++ libs/admin-ui/src/lib/admin-ui.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@cleric-eshop/material';
 import { NgZorroModule } from '@cleric-eshop/ng-zorro';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { CategorySelectorComponent } from './components/category-selector/category-selector.component';
 
 @NgModule({
   imports: [CommonModule, MaterialModule, NgZorroModule, RouterModule],
-  declarations: [SidenavComponent],
-  exports: [SidenavComponent]
+  declarations: [SidenavComponent, CategorySelectorComponent],
+  exports: [SidenavComponent, CategorySelectorComponent]
 })
 export class AdminUiModule {}
---
+++ libs/admin-ui/src/lib/components/category-selector/category-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cleric-eshop-category-selector',
+  templateUrl: './category-selector.component.html',
+  styleUrls: ['./category-selector.component.scss']
+})
+export class CategorySelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
